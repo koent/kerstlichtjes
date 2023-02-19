5c36c5be4a82eb24184b40518a821530830253b2
koent <ktbbb5@gmail.com>
2023-02-19 15:55:34 +0100

    Only deploy if build succeeded

--- build/buildAndDeploy.sh
+++ build/buildAndDeploy.sh
@@ -5,5 +5,5 @@ cd $rootDir/..
 . "build/customVariables.sh"
 
 nuget restore $solutionName
-msbuild $solutionName /p:NanoFrameworkProjectSystemPath="$nanoFrameworkPath"/dist/utils/nanoFramework/v1.0/
+msbuild $solutionName /p:NanoFrameworkProjectSystemPath="$nanoFrameworkPath"/dist/utils/nanoFramework/v1.0/ && 
 mono $nanoFrameworkPath/dist/utils/nanoFrameworkDeployer/nanoFrameworkDeployer.exe -v -c $serialPort -d Kerstlichtjes/bin/Debug
